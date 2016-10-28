802ff52601eea7aeabf04bbd435179b080341272
Andreas Müller <schnitzeltony@googlemail.com>
2016-10-28 14:34:36 +0200

    usb: musb: really fix tx fifo flush handling
    Signed-off-by: Andreas Müller <schnitzeltony@googlemail.com>

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -112,18 +112,15 @@ static void musb_h_tx_flush_fifo(struct musb_hw_ep *ep)
 	struct musb	*musb = ep->musb;
 	void __iomem	*epio = ep->regs;
 	u16		csr;
-	u16		lastcsr = 0;
 	int		retries = 1000;
 
 	csr = musb_readw(epio, MUSB_TXCSR);
 	while (csr & MUSB_TXCSR_FIFONOTEMPTY) {
-		if (csr != lastcsr)
-			dev_dbg(musb->controller, "Host TX FIFONOTEMPTY csr: %02x\n", csr);
-		lastcsr = csr;
+		csr &= ~MUSB_TXCSR_FIFONOTEMPTY;
 		csr |= MUSB_TXCSR_FLUSHFIFO | MUSB_TXCSR_TXPKTRDY;
 		musb_writew(epio, MUSB_TXCSR, csr);
 		csr = musb_readw(epio, MUSB_TXCSR);
-		if (WARN(retries-- < 1,
+		if (dev_WARN_ONCE(musb->controller, retries-- < 1,
 				"Could not flush host TX%d fifo: csr: %04x\n",
 				ep->epnum, csr))
 			return;
